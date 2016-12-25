a14f4c94accee607f4a349efd79253630a6fc02e
Hugo WEBER <huweber@e2r2p11.42.fr>
2016-12-25 18:54:18 +0100

    ajout liste chainees

--- huweber/fillit.c
+++ huweber/fillit.c
@@ -6,80 +6,40 @@
 /*   By: huweber <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/17 16:20:15 by huweber           #+#    #+#             */
-/*   Updated: 2016/12/25 16:29:00 by huweber          ###   ########.fr       */
+/*   Updated: 2016/12/25 18:36:05 by huweber          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include "libft.h"
 
-void			ft_parse_tetri(char	*buff, t_tmp t)
-{
-	(void)buff;
-	(void)t;
-}
-
-t_tmp			ft_var_init(char *buff)
-{
-	t_tmp		t;
-
-	t.len = ft_strlen(buff);
-	t.i = 0;
-	t.count = 0;
-	t.square = 0;
-	t.line = -1;
-	if (((t.len + 1) % 21) != 0)
-		print_err(0);
-	return (t);
-}
-
-void			ft_basic_verifs(char *buff)
-{
-	t_tmp		t;
-
-	t = ft_var_init(buff);
-	while (t.i < t.len)
-	{
-		if (buff[t.i] == '\n' && t.line == 4)
-		{
-			t.square++;
-			t.i++;
-		}
-		if (buff[t.i] == '\n' && t.square == 4)
-		{
-			t.square = 0;
-			t.count++;
-			t.i++;
-		}
-		t.line = -1;
-		while (++t.line < 4)
-		{
-			if (buff[t.i] != '#' && buff[t.i] != '.' && t.i < t.len)
-				print_err(0);
-			t.i++;
-		}
-	}
-	ft_parse_tetri(buff, t);
-}
-
-void			ft_init(char *file)
+void			list_init(char *file, t_tetri *t)
 {
 	int			fd;
-	char		*buff;
+	t_list		*last;
+	t_list		**pivot;
+	t_list		*head;
+	char		*tmp;
 
+	last = NULL;
+	pivot = &last;
 	if ((fd = open(file, O_RDONLY)) == -1)
 		print_err(0);
-	if (!(buff = (char *)malloc(BUFF_SIZE * sizeof(char))))
+	if (!(tmp = (char *)malloc(BUFF_SIZE * sizeof(char))))
 		print_err(0);
-	if (read(fd, buff, BUFF_SIZE) == -1)
-		print_err(0);
-	ft_basic_verifs(buff);
+	while (read(fd, tmp, 21) >= 20)
+		ft_lstadd(&last, ft_lstnew(tmp, 20));
+	head = *pivot;
+	t->size = ft_lstsize(head);
+	free(tmp);
 }
 
 int				main(int ac, char **av)
 {
+	t_tetri		t;
+
 	if (ac != 2)
 		print_err(1);
-	ft_init(av[1]);
+	list_init(av[1], &t);
 	return (0);
 }
--- huweber/fillit.h
+++ huweber/fillit.h
@@ -6,7 +6,7 @@
 /*   By: huweber <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/17 16:22:48 by huweber           #+#    #+#             */
-/*   Updated: 2016/12/25 16:20:21 by huweber          ###   ########.fr       */
+/*   Updated: 2016/12/25 18:30:02 by huweber          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,10 @@
 # define BUFF_SIZE 700
 
 void			print_err(int flag);
-void			ft_init(char *file);
 
-typedef struct		s_tmp
+typedef struct      s_tetri
 {
-	int				i;
-	int				len;
-	int				line;
-	int				count;
-	int				square;
-}					t_tmp;
-
-typedef struct		s_parse
-{
-	int				count;
-	char			**grid;
-}					t_parse;
+	int				size;
+}					t_tetri;
 
 #endif
